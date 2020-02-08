a17fe721497bbd13fa5467e282a0ef77c5b296ff
叶枫 <645381995@qq.com>
2020-02-08 08:22:49 +0800

    0.0.4

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,6 +3,7 @@ import { IBundleOptions } from 'father';
 const options: IBundleOptions = {
   // cjs: 'rollup',
   esm: 'rollup',
+  runtimeHelpers: true,
   extraBabelPlugins: [
     ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
